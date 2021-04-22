1434e8aa5da4731117d899ce26432568e6557dbc
Kawshik Banerjee <k.banerjee2024@gmail.com>
2021-04-22 19:09:20 +0600

    initial-commit

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+# it's a bot module to map the URLs to correspond to each view function
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+]
