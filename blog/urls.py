afe72b9d493f0e482a3f5c5ea05935fa4e47911f
Kawshik Banerjee <k.banerjee2024@gmail.com>
2021-07-18 02:20:30 +0600

    user's all post filtering + pagination update

--- blog/urls.py
+++ blog/urls.py
@@ -1,20 +1,21 @@
 # it's a bot module to map the URLs to correspond to each view function
-
 from django.urls import path
 from .views import (
     PostListView, 
     PostDetailView, 
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView
 )
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('about/', views.about, name='blog-about'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
-    path('about/', views.about, name='blog-about'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
 ]
